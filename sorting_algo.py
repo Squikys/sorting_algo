a217c9eb852c59f45702eb185ffb61f66fd542ba
Squikys <kaustavmahata@gmail.com>
2023-07-17 22:51:00 +0530

    this is the final commit

--- sorting_algo.py
+++ sorting_algo.py
@@ -4,19 +4,18 @@ txt_is_found = False
 pdf_is_found = False
 psd_is_found = False
 ai_is_found = False
-def file (fileType,folderName):
-    if fileType in file_list[files]:
-        if not(pdf_is_found) :
-            os.mkdir(folderName)
-            pdf_is_found = True
-        newpath = folderName+'/'+file_list[files]
-        pathlib.Path(file_list[files]).rename(newpath)
+
 
 dir = os.getcwd()
 print(dir)
 file_list = os.listdir(dir)
 for files in range(0,list.__len__(file_list)):
-    file(".pdf","pdf")
+    if ".pdf" in file_list[files]:
+        if not(pdf_is_found) :
+            os.mkdir("pdf")
+            pdf_is_found = True
+        newpath = 'pdf/'+file_list[files]
+        pathlib.Path(file_list[files]).rename(newpath)
     if ".psd" in file_list[files]:
         if not(psd_is_found) :
             os.mkdir("psd")
