d47c820720af686ba429654f3ba6d226152b71ba
Squiky <116367384+Squikys@users.noreply.github.com>
2023-07-22 00:18:21 +0530

    work complete

--- sorting_algo.py
+++ sorting_algo.py
@@ -1,36 +1,16 @@
 import os
 import pathlib
-txt_is_found = False
-pdf_is_found = False
-psd_is_found = False
-ai_is_found = False
 
+file_list = os.listdir(os.getcwd())
 
-dir = os.getcwd()
-print(dir)
-file_list = os.listdir(dir)
-for files in range(0,list.__len__(file_list)):
-    if ".pdf" in file_list[files]:
-        if not(pdf_is_found) :
-            os.mkdir("pdf")
-            pdf_is_found = True
-        newpath = 'pdf/'+file_list[files]
-        pathlib.Path(file_list[files]).rename(newpath)
-    if ".psd" in file_list[files]:
-        if not(psd_is_found) :
-            os.mkdir("psd")
-            psd_is_found = True
-        newpath = 'psd/'+file_list[files]
-        pathlib.Path(file_list[files]).rename(newpath)
-    if ".ai" in file_list[files]:
-        if not(ai_is_found) :
-            os.mkdir("ai")
-            ai_is_found = True
-        newpath = 'ai/'+file_list[files]
-        pathlib.Path(file_list[files]).rename(newpath)
-    if ".txt" in file_list[files]:
-        if not(txt_is_found) :
-            os.mkdir("txt")
-            txt_is_found = True
-        newpath = 'txt/'+file_list[files]
-        pathlib.Path(file_list[files]).rename(newpath)
+for a in range(len(file_list)):
+    if file_list[a] != 'sorting_algo.py' and '.' in file_list[a]:
+        worker = file_list[a]
+        i=len(worker.split('.'))
+        try:
+            os.mkdir(worker.split('.')[i-1])
+        except:
+            pass    
+        newpath = worker.split('.')[i-1]+'/'+file_list[a]
+        pathlib.Path(file_list[a]).rename(newpath)
+        
